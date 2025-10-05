edbc79af90264c79e4fa59f7e9326998c155370b
FrekeNobles <noblendifreke@gmail.com>
2025-10-05 21:03:12 +0100

    feat.:create training feedback page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ConferenceHall from './pages/ConferenceHall';
 import BusinessSolutions from './pages/BusinessSolutions';
 import Landingpage from './pages/Landingpage';
+import Trainingfbform from './pages/Trainingfbform';
 
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Landingpage />} />
-         <Route path="/conference" element={<ConferenceHall/>}/>
+        <Route path="/conference" element={<ConferenceHall/>}/>
         <Route path="/business" element={<BusinessSolutions/>}/>
+        <Route path="/training" element={<Trainingfbform/>} />
       </Routes>
     </Router>
     
